5ff0b45b135ab6886c8e4fafa0b03344e0023fc2
Vitor Baradelli <vitorbaradelli@gmail.com>
2023-02-24 11:47:43 -0300

    second class finished

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
 import './styles/global.css'
 
-function App() {
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
+
+export function App() {
   return (
-    <div className="bg-zinc-300 w-10/12">
-      <h1>hola</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
